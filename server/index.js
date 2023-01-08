e326fb4aeb2b261a4880ebde0a408a50dd3edf1c
Doziem <agbokennethchidoziem@gmail.com>
2023-01-08 05:58:18 +0100

    created the post and user with all the functionalities

--- server/index.js
+++ server/index.js
@@ -8,8 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import authRoutes from "./routes/auth"
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,9 +43,13 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/post", verifyToken, upload.single("picture"), createPost);
 
 // Routers
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
 
 /* MONGOOSE SETUP */
 mongoose.set('strictQuery', true);
