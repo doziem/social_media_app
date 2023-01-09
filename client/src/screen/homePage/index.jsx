345fba508e905aa122959d0a24bfcaff5fd180e4
Doziem <agbokennethchidoziem@gmail.com>
2023-01-09 09:10:46 +0100

    Done with nav bar

--- client/src/screen/homePage/index.jsx
+++ client/src/screen/homePage/index.jsx
@@ -1,5 +1,12 @@
+import { Box } from '@mui/material';
+import Navbar from 'screen/navbar';
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default HomePage;
