5eaca0161ee816cc54c5f5ff116501ed3d8b20df
Doziem <agbokennethchidoziem@gmail.com>
2023-01-09 13:30:36 +0100

    register and login implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./screen/homePage";
 import LoginPage from "./screen/loginPage";
 import ProfilePage from "./screen/profilePage";
@@ -14,6 +14,9 @@ function App() {
 
   const mode = useSelector(state => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode), [mode]))
+
+  const isAuth = Boolean(useSelector((state) => state.token))
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -21,8 +24,8 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
           </Routes>
 
         </ThemeProvider>
